cefa1d602168fe46fc47ffd906f0279c7673a779
inbary-s <inbarsim6@gmail.com>
2020-02-08 16:19:28 -0700

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -5,12 +5,10 @@ if(process.env.JAWSDB_URL){
 }else{
  connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
     password: "Bb>T00t@6",
     database: "burgers_DB"
   });
 };
   connection.connect();
-
   module.exports = connection;
