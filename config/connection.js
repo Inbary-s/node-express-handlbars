b3deb1b5edfec1dd60c744f756cb9c77ee47db90
inbary-s <inbarsim6@gmail.com>
2020-02-08 16:16:42 -0700

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 let connection;
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{}
+}else{
  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -10,7 +10,7 @@ if(process.env.JAWSDB_URL){
     password: "Bb>T00t@6",
     database: "burgers_DB"
   });
-
+};
   connection.connect();
 
   module.exports = connection;
